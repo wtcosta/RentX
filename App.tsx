e7f78d0a087072bf2daec0b923c836f3988d8acf
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-06-23 20:32:04 -0300

    fonts e estilo padrão do thema

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo'
+import {
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+
+import theme from './src/styles/theme';
+import { Home } from './src/scrrens/Home';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium
+  })
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
--- src/scrrens/Home/index.tsx
+++ src/scrrens/Home/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import {
-    Container
+    Container,
+    Title
 } from './styles'
 
 export function Home(){
     return (
         <Container>
-
+            <Title>Home</Title>
         </Container>
     )
 }
