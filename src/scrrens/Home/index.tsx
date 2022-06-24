fe1e0b3dd63f71b521bfe8ccae355eb2a2aec02f
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-06-24 01:14:18 -0300

    list de carros

--- src/scrrens/Home/index.tsx
+++ src/scrrens/Home/index.tsx
@@ -4,14 +4,27 @@ import { RFValue } from 'react-native-responsive-fontsize'
 
 import Logo from '../../assets/logo.svg'
 
+import { Car } from '../../components/Car'
+
 import {
     Container,
     Header,
     HeaderContent,
     TotalCars,
+    CarList,
 } from './styles'
 
 export function Home(){
+    const carData = {
+        brand: 'Audi',
+        name: 'RS 5 Coupé',
+        rent: {
+            period: 'AO DIA',
+            price: 120,
+        },
+        thumbnail: 'https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png',
+    }
+
     return (
         <Container>
             <StatusBar
@@ -28,6 +41,13 @@ export function Home(){
                     <TotalCars>Total de 12 carros</TotalCars>
                 </HeaderContent>
             </Header>
+
+            <CarList
+                data={[1,2,3,4,5,6,7,8,9]}
+                keyExtractor={item => String(item)}
+                renderItem={({item}) => <Car data={carData}/>}
+            />
+
         </Container>
     )
 }
