b19044c2c61d3af388c51d2766160ec0e246a097
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-07-05 03:38:44 -0300

    fix: ajuste de tipagem

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native'
 import { RFValue } from 'react-native-responsive-fontsize';
-import { FlatList } from 'react-native'
+import { FlatList, FlatListProps } from 'react-native'
 import { CarDTO } from '../../dtos/CarDTO'
 
 export const Container = styled.View`
