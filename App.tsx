da68b400210e86552385238e67ae4985a3c77483
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-07-05 00:55:52 -0300

    interface detalhes do agendamento

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
 import theme from './src/styles/theme';
 import { Home } from './src/scrrens/Home';
 
-import { Scheduling } from './src/scrrens/Scheduling'
+import { SchedulingDetails } from './src/scrrens/SchedulingDetails'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +31,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <SchedulingDetails />
     </ThemeProvider>
   );
 }
---
+++ src/scrrens/SchedulingDetails/index.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import { Feather } from '@expo/vector-icons'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { useTheme } from 'styled-components'
+
+import { BackButton } from '../../components/BackButton'
+import { ImageSlider } from '../../components/ImageSlider'
+import { Accessory } from '../../components/Accessory'
+import { Button } from '../../components/Button'
+
+import speedSvg from '../../assets/speed.svg'
+import accelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
+
+import {
+    Container,
+    Header,
+    CarImages,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    Acessories,
+    Footer,
+    RentalPeriod,
+    CalendarIcon,
+    DateInfo,
+    DateTitle,
+    DateValue,
+    RentalPrice,
+    RentalPriceLabel,
+    RentalPriceDetails,
+    RentalPriceQuota,
+    RentalPriceTotal,
+} from './styles'
+
+export function SchedulingDetails(){
+    const theme = useTheme()
+
+    return (
+        <Container>
+            <Header>
+                <BackButton onPress={() => {}}/>
+            </Header>
+            <CarImages>
+                <ImageSlider
+                    imagesUrl={['https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png']}
+                />
+            </CarImages>
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Hucan</Name>
+                    </Description>
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+
+                <Acessories>
+                    <Accessory name="380Km/h" icon={speedSvg}/>
+                    <Accessory name="3.2s" icon={accelerationSvg}/>
+                    <Accessory name="800 HP" icon={forceSvg}/>
+                    <Accessory name="Gasolina" icon={gasolineSvg}/>
+                    <Accessory name="Auto" icon={exchangeSvg}/>
+                    <Accessory name="2 pessoas" icon={peopleSvg}/>
+                </Acessories>
+
+                <RentalPeriod>
+                    <CalendarIcon>
+                        <Feather
+                            name="calendar"
+                            size={RFValue(24)}
+                            color={theme.colors.shape}
+                        />
+                    </CalendarIcon>
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+                        <DateValue>18/06/2021</DateValue>
+                    </DateInfo>
+
+                    <Feather
+                        name="chevron-right"
+                        size={RFValue(10)}
+                        color={theme.colors.shape}
+                    />
+
+                    <DateInfo>
+                        <DateTitle>ATÉ</DateTitle>
+                        <DateValue>18/06/2021</DateValue>
+                    </DateInfo>
+                </RentalPeriod>
+
+                <RentalPrice>
+                    <RentalPriceLabel>Total</RentalPriceLabel>
+                    <RentalPriceDetails>
+                        <RentalPriceQuota>R$ 580 x3 diarias</RentalPriceQuota>
+                        <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+                    </RentalPriceDetails>
+                </RentalPrice>
+            </Content>
+
+            <Footer>
+                <Button title="Confirmar" />
+            </Footer>
+        </Container>
+    )
+}
