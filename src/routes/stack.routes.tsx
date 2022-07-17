e4598cd883f9d4ae9f787933fd1f34adaceabfbc
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-07-16 22:38:17 -0300

    splash animada

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,14 +7,22 @@ import { Scheduling } from '../screens/Scheduling'
 import { SchedulingDetails } from '../screens/SchedulingDetails'
 import { SchedelingComplete } from '../screens/SchedelingComplete'
 import { MyCars } from '../screens/MyCars'
+import { Splash } from '../screens/Splash'
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes(){
     return(
-        <Navigator screenOptions={{
-            headerShown: false
-        }}>
+        <Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+            initialRouteName="Splash"
+        >
+            <Screen
+                name="Splash"
+                component={Splash}
+            />
             <Screen
                 name="Home"
                 component={Home}
