c64fe1375501646b3cb668674ea33002548b8bad
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-07-16 23:52:52 -0300

    animando btn meus agendamentos

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { StatusBar } from 'react-native'
+import { StatusBar, StyleSheet } from 'react-native'
 import { RFValue } from 'react-native-responsive-fontsize'
 import { ParamListBase, NavigationProp, useNavigation } from '@react-navigation/native'
 import { Ionicons } from '@expo/vector-icons'
 import { useTheme } from 'styled-components'
+import { RectButton, PanGestureHandler } from 'react-native-gesture-handler'
+
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    useAnimatedGestureHandler,
+    withSpring
+} from 'react-native-reanimated'
+
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton)
 
 import Logo from '../../assets/logo.svg'
 import { api } from '../../services/api'
@@ -17,16 +27,41 @@ import {
     Header,
     HeaderContent,
     TotalCars,
-    CarList,
-    MyCarsButton
+    CarList
 } from './styles'
 
 export function Home(){
     const [cars, setCars] = useState<CarDTO[]>([])
     const [loading, setLoading] = useState(true)
-    const navigation = useNavigation<NavigationProp<ParamListBase>>();
 
+    const navigation = useNavigation<NavigationProp<ParamListBase>>();
     const theme = useTheme()
+    const positionY = useSharedValue(0)
+    const positionX = useSharedValue(0)
+
+    const myCarsButtonStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {translateX: positionX.value},
+                {translateY: positionY.value},
+            ]
+        }
+    })
+
+    const onGestureEvent = useAnimatedGestureHandler({
+        onStart(_, ctx: any){
+            ctx.positionX = positionX.value
+            ctx.positionY = positionY.value
+        },
+        onActive(event, ctx: any){
+            positionX.value = ctx.positionX + event.translationX
+            positionY.value = ctx.positionY + event.translationY
+        },
+        onEnd(){
+            // positionX.value = withSpring(0)
+            // positionY.value = withSpring(0)
+        }
+    })
 
     function handleCarDetails(car: CarDTO){
         navigation.navigate('CarDetails', {car})
@@ -76,14 +111,43 @@ export function Home(){
                 />
             }
 
-            <MyCarsButton onPress={handleOpenMyCars}>
-                <Ionicons
-                    name="ios-car-sport"
-                    size={32}
-                    color={theme.colors.shape}
-                />
-            </MyCarsButton>
+            <PanGestureHandler onGestureEvent={onGestureEvent}>
+                <Animated.View
+                    style={[
+                        myCarsButtonStyle,
+                        {
+                            position: 'absolute',
+                            bottom: 13,
+                            right: 22,
+                        }
+                    ]}
+                >
+                    <ButtonAnimated
+                        onPress={handleOpenMyCars}
+                        style={[
+                            styles.button,
+                            { backgroundColor: theme.colors.main }
+                        ]}
+                    >
+                        <Ionicons
+                            name="ios-car-sport"
+                            size={32}
+                            color={theme.colors.shape}
+                        />
+                    </ButtonAnimated>
+                </Animated.View>
+            </PanGestureHandler>
 
         </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -35,15 +35,3 @@ export const CarList = styled(FlatList as new (props: FlatListProps<CarDTO>) => 
     },
     showVerticalScrollIndicator: false
 })``
-
-export const MyCarsButton = styled(RectButton)`
-    width: 60px;
-    height: 60px;
-    background-color: ${({theme}) => theme.colors.main};
-    justify-content: center;
-    align-items: center;
-    border-radius: 30px;
-    position: absolute;
-    bottom: 13px;
-    right: 22px;
-`
